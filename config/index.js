479f094e724bc24aa0767e7c50c99559d3014ed6
yelu <718611101@qq.com>
2020-09-23 11:29:02 +0800

    提交

--- config/index.js
+++ config/index.js
@@ -1,43 +1,70 @@
 const config = {
-  projectName: 'ninghao-taro',
-  date: '2020-9-20',
+  projectName: 'demo',
+  date: '2018-8-28',
   designWidth: 750,
   deviceRatio: {
-    640: 2.34 / 2,
-    750: 1,
-    828: 1.81 / 2
+    '640': 2.34 / 2,
+    '750': 1,
+    '828': 1.81 / 2
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: [],
+  plugins: {
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'transform-class-properties',
+        'transform-decorators-legacy',
+        'transform-object-rest-spread'
+      ]
+    },
+    typescript: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: true,
+        baseUrl: '.',
+        declaration: false,
+        experimentalDecorators: true,
+        jsx: 'preserve',
+        jsxFactory: 'Nerv.createElement',
+        module: 'commonjs',
+        moduleResolution: 'node',
+        noImplicitAny: false,
+        noUnusedLocals: true,
+        outDir: './dist/',
+        preserveConstEnums: true,
+        removeComments: false,
+        rootDir: '.',
+        sourceMap: true,
+        strictNullChecks: true,
+        target: 'es6'
+      },
+      include: [
+        'src/**/*'
+      ],
+      exclude: [
+        'node_modules'
+      ],
+      compileOnSave: false
+    }
+  },
   defineConstants: {
   },
   copy: {
-    patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {}
   },
-  framework: 'react',
-  mini: {
-    postcss: {
-      pxtransform: {
-        enable: true,
-        config: {
-
-        }
-      },
-      url: {
-        enable: true,
-        config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+  weapp: {
+    module: {
+      postcss: {
+        autoprefixer: {
+          enable: true
+        },
+        url: {
+          enable: true,
+          limit: 10240
         }
       }
     }
@@ -45,17 +72,11 @@ const config = {
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    postcss: {
-      autoprefixer: {
-        enable: true,
-        config: {
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+    esnextModules: ['taro-ui'],
+    module: {
+      postcss: {
+        autoprefixer: {
+          enable: true
         }
       }
     }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,56 @@
-import { Component } from 'react'
-import './app.less'
+import Taro, { Component } from '@tarojs/taro'
+import Index from './pages/index/index'
+
+import './app.scss'
 
 class App extends Component {
+  config = {
+    pages: [
+      'pages/index/index',
+      'pages/basic/index',
+      'pages/view/index',
+      'pages/view/article/index',
+      'pages/feedback/index',
+      'pages/data-entry/index',
+      'pages/layout/index',
+      'pages/navigation/index',
+      'pages/navigation/indexes/index',
+      'pages/advanced/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#000',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'white'
+    },
+    tabBar: {
+      color: "#666",
+      selectedColor: "#b4282d",
+      backgroundColor: "#fafafa",
+      borderStyle: 'black',
+      list: [{
+        pagePath: "pages/index/index",
+        iconPath: "/assets/images/curtain.png",
+        selectedIconPath: "/assets/images/curtain.png",
+        text: "首页"
+      }, {
+        pagePath: "pages/basic/index",
+        iconPath:"/assets/images/curtain.png",
+        selectedIconPath:"/assets/images/curtain.png",
+        text: "分类"
+      }, {
+        pagePath: "pages/view/article/index",
+        iconPath: "/assets/images/curtain.png",
+        selectedIconPath:"/assets/images/curtain.png",
+        text: "购物车"
+      }, {
+        pagePath: "pages/advanced/index",
+        iconPath:"/assets/images/curtain.png",
+        selectedIconPath: "/assets/images/curtain.png",
+        text: "个人"
+      }]
+    }
+  }
 
   componentDidMount () {}
 
@@ -9,12 +58,11 @@ class App extends Component {
 
   componentDidHide () {}
 
-  componentDidCatchError () {}
+  componentCatchError () {}
 
-  // this.props.children 是将要会渲染的页面
   render () {
-    return this.props.children
+    return <Index />
   }
 }
 
-export default App
+Taro.render(<App />, document.getElementById('app'));
